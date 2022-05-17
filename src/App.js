dabbb12587a650ec4c6a864c33927fad87c2692d
Jessica Fenna <jessicafenna@gmail.com>
2022-05-17 13:31:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'; 
+import { evaluate } from 'mathjs';
 
-function App() {
+const App = () =>{
+  let [result,setResult] = useState("");
+
+  // const operators = ["/","*","-,","+","."]
+
+  const updateResult = value => { 
+    setResult(result+value);
+  }
+  const calculate = () => { 
+    setResult(evaluate(result).toString());
+  }
+  const del = () => { 
+    const value = result.slice(0,-1);
+    setResult(value)
+  }
+
+  const delAll = () => { 
+    const value = ("0")
+    setResult(value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex'>
+    <div className = "container">
+      <div className ="screen">
+          <h1>{result || "0"}</h1>
+      </div> 
+          <div>
+            <button onClick = {del} >Backspace</button>
+            <button onClick = {delAll} >Clear All</button>
+        
+            
+           </div>
+
+          <div className = "numberButtons">
+            <div>
+              <button onClick ={() => updateResult ('7')}>7</button>
+              <button onClick ={() => updateResult ('8')}>8</button>
+              <button onClick ={() => updateResult ('9')}>9</button>
+              <button onClick = {() => updateResult("/")} >*</button>
+           
+            </div>
+            <div>
+              <button onClick ={() => updateResult ('4')}>4</button>
+              <button onClick ={() => updateResult ('5')}>5</button>
+              <button onClick ={() => updateResult ('6')}>6</button>
+              <button onClick = {() => updateResult("-")} >-</button>
+          
+            </div>
+            <div>         
+              <button onClick ={() => updateResult ('1')}>1</button>
+              <button onClick ={() => updateResult ('2')}>2</button>
+              <button onClick ={() => updateResult ('3')}>3</button>
+              <button onClick = {() => updateResult("+")} >+</button>
+             
+            </div>
+            <div>
+              <button onClick ={() => updateResult ('0')}>0</button>
+              <button onClick = {() => updateResult(".")} >.</button>              
+              <button onClick = {() => updateResult("/")} >/</button>
+              <button onClick ={calculate}>=</button>              
+          </div>
     </div>
-  );
+    </div>
+    </div>
+  )
 }
 
 export default App;
