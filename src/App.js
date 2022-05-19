0df66e0795dc522f2c0e4d8cb0447cfeeba006b7
Jessica Fenna <jessicafenna@gmail.com>
2022-05-19 18:46:04 +0100

    now with mapping!

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { useState } from 'react';
 import { evaluate } from 'mathjs';
 
 const App = () =>{
-  let [result,setResult] = useState("");
+  const [result, setResult] = useState ("")
+  const numbers = [7, 8, 9, "/",4, 5, 6, "*",1, 2, 3, "-",0, ".", "+", "=", ]
 
-  const updateResult = value => { 
-    setResult(result+value);
-  }
-  const total = () => { 
-    setResult(evaluate(result));
-  }
+  const clickHandler = (button) =>{ 
+    setResult (button === "=" ? evaluate (result) : button === "C" ? "" : result + button
+    )}
+
+
   const del = () => { 
     const value = result.slice(0,-1);
     setResult(value)
@@ -34,32 +34,12 @@ const App = () =>{
            </div>
 
           <div>
-            <div>
-              <button onClick ={() => updateResult ('7')}>7</button>
-              <button onClick ={() => updateResult ('8')}>8</button>
-              <button onClick ={() => updateResult ('9')}>9</button>
-              <button onClick = {() => updateResult("/")} >*</button>
-           
-            </div>
-            <div>
-              <button onClick ={() => updateResult ('4')}>4</button>
-              <button onClick ={() => updateResult ('5')}>5</button>
-              <button onClick ={() => updateResult ('6')}>6</button>
-              <button onClick = {() => updateResult("-")} >-</button>
-          
-            </div>
-            <div>         
-              <button onClick ={() => updateResult ('1')}>1</button>
-              <button onClick ={() => updateResult ('2')}>2</button>
-              <button onClick ={() => updateResult ('3')}>3</button>
-              <button onClick = {() => updateResult("+")} >+</button>
-             
-            </div>
-            <div>
-              <button onClick ={() => updateResult ('0')}>0</button>
-              <button onClick = {() => updateResult(".")} >.</button>              
-              <button onClick = {() => updateResult("/")} >/</button>
-              <button onClick ={total}>=</button>              
+            <div> {numbers.map((button,index) =>{ 
+          return (
+          <button onClick= {() => clickHandler(button)} key = {index}>{button}</button>
+          )
+        })}
+              
           </div>
     </div>
     </div>
